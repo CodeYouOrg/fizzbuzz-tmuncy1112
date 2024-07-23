33c0d0ccd84284771343edff2f5aa4a6d4cce6b5
Your Name <you@example.com>
2024-07-23 16:00:18 -0400

    fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,13 @@
 # add your code here
-
+def fizzbuzz_if_else(n):
+    nums = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            nums.append("FizzBuzz")
+        elif i % 3 == 0:
+            nums.append("Fizz")
+        elif i % 5 == 0:
+            nums.append("Buzz")
+        else:
+            nums.append(i)
+    return "\n".join(map(str, nums))
